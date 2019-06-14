71951228f867802484dc133bcd33147328751e69
Adriana M <adrianaymontilla@gmail.com>
2019-06-14 11:47:53 -0300

    feat: form input y boton

---
+++ src/Components/Form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Input from 'Input';
+import Button from 'Buttom';
+
+class Form extends React.Component {
+    render() {
+        return (
+            <div className="form">
+                <form>
+                    <Input></Input>
+                    <Button></Button>
+                </form>
+               
+                
+            </div>
+        )
+    }
+}
+
+export default Form;
---
+++ src/Components/Input.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Input extends React.Component {
+    render() {
+        return (
+            <div className="form">
+                <form>
+                    <input type="text" value="Nombre de usuario"></input>
+                </form>
+                
+                
+            </div>
+        )
+    }
+}
+
+export default Input;
