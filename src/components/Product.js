5a0dffb6c0553b612d20f0830b61139feb308577
Lourdes Rosales <lourdesrosales1997@gmail.com>
2019-06-25 15:47:00 -0300

    components

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-
-
 function ProductList() {
     return (
       <div>
-          <div className="box_component_product">
-
+          <div>
           </div>
       </div>
     );
