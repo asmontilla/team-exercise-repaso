3f37fe7961b3c9f96f56478d17e2054c8059e248
Lourdes Rosales <lourdesrosales1997@gmail.com>
2019-07-02 19:32:44 -0300

    merge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import './App.css';
-import Form from './Components/Form';
-
-function App() {
-  return (
-    <div className="App">
-      <Form></Form>
-    </div>
-  );
-=======
 import Form from './components/Form'
 
 class App extends Component {
@@ -20,7 +9,6 @@ class App extends Component {
             </div>
         );
     }
->>>>>>> cbfd6aa930fe7484bcac7c441f10390c2daa8831
 }
 
 export default App;
